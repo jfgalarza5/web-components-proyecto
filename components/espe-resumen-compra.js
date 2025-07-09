d7d2039824deb89ebcb5ae651868f546ab985e69
johngalarza1970 <johngalarza1970@gmail.com>
2025-07-09 16:30:48 -0500

    feat: agregar propiedades y constructor al componente

--- components/espe-resumen-compra.js
+++ components/espe-resumen-compra.js
@@ -1,6 +1,21 @@
 import { LitElement, html } from 'lit';
 
 class ESPEResumenCompra extends LitElement {
+  static get properties() {
+    return {
+      productos: { type: Number },
+      envio: { type: Number },
+      impuestos: { type: Number }
+    };
+  }
+
+  constructor() {
+    super();
+    this.productos = 0;
+    this.envio = 0;
+    this.impuestos = 0;
+  }
+
   render() {
     return html`<p>Resumen de compra</p>`;
   }
