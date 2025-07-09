c5bf5203919771d519c8a1053b7493615c324b87
johngalarza1970 <johngalarza1970@gmail.com>
2025-07-09 17:27:40 -0500

    feat: agregar propiedades y valores por defecto

--- components/espe-envio-gratis/espe-envio-gratis.js
+++ components/espe-envio-gratis/espe-envio-gratis.js
@@ -1,8 +1,23 @@
 import { LitElement, html } from 'lit';
 
 class ESPEEnvioGratis extends LitElement {
+  static get properties() {
+    return {
+      totalActual: { type: Number },
+      totalGratis: { type: Number },
+      tema: { type: String }
+    };
+  }
+
+  constructor() {
+    super();
+    this.totalActual = 0;
+    this.totalGratis = 50;
+    this.tema = 'claro';
+  }
+
   render() {
-    return html`<p>Componente de envío gratis</p>`;
+    return html`<p>Total actual: ${this.totalActual}</p>`;
   }
 }
 
