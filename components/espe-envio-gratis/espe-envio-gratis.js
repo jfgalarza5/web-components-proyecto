3466a2a0785261b12bcfea074eb68982bf229046
johngalarza1970 <johngalarza1970@gmail.com>
2025-07-09 17:27:27 -0500

    feat: crear componente base espe-envio-gratis

---
+++ components/espe-envio-gratis/espe-envio-gratis.js
@@ -0,0 +1,9 @@
+import { LitElement, html } from 'lit';
+
+class ESPEEnvioGratis extends LitElement {
+  render() {
+    return html`<p>Componente de envío gratis</p>`;
+  }
+}
+
+customElements.define('espe-envio-gratis', ESPEEnvioGratis);
