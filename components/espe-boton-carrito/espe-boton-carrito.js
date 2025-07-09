1b74a858bcc64ad6f8484f5a7d699d1993921ecb
johngalarza1970 <johngalarza1970@gmail.com>
2025-07-09 18:48:31 -0500

    feat: agregar soporte para temas claro y oscuro

--- components/espe-boton-carrito/espe-boton-carrito.js
+++ components/espe-boton-carrito/espe-boton-carrito.js
@@ -14,9 +14,28 @@ class ESPEBotonCarrito extends LitElement {
     this.tema = 'claro';
   }
 
+  updated() {
+    this._aplicarTema();
+  }
+
+  _aplicarTema() {
+    const s = this.style;
+    if (this.tema === 'oscuro') {
+      s.setProperty('--fondo-icono', '#2e7d32');
+      s.setProperty('--color-icono', '#ffffff');
+      s.setProperty('--color-badge', '#43a047');
+    } else {
+      s.setProperty('--fondo-icono', '#e0f2f1');
+      s.setProperty('--color-icono', '#2e7d32');
+      s.setProperty('--color-badge', '#2e7d32');
+    }
+  }
+
   render() {
     return html`
-      <div>Boton carrito</div>
+      <div style="background-color: var(--fondo-icono); color: var(--color-icono); padding: 10px; border-radius: 50%;">
+        Boton carrito
+      </div>
     `;
   }
 }
