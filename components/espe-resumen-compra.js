19d73a814a4e6a5d814e27e172c728bd24b1765b
johngalarza1970 <johngalarza1970@gmail.com>
2025-07-09 16:31:44 -0500

    feat: añadir calculo de total y formateo decimal

--- components/espe-resumen-compra.js
+++ components/espe-resumen-compra.js
@@ -16,13 +16,18 @@ class ESPEResumenCompra extends LitElement {
     this.impuestos = 0;
   }
 
+  calcularTotal() {
+    return `$${(this.productos + this.envio + this.impuestos).toFixed(2).replace('.', ',')}`;
+  }
+
   render() {
     return html`
       <div>
         <h2>Resumen de compra</h2>
-        <p>Productos: $${this.productos.toFixed(2)}</p>
-        <p>Impuestos: $${this.impuestos.toFixed(2)}</p>
-        <p>Envío: $${this.envio.toFixed(2)}</p>
+        <p>Productos: $${this.productos.toFixed(2).replace('.', ',')}</p>
+        <p>Impuestos: $${this.impuestos.toFixed(2).replace('.', ',')}</p>
+        <p>Envío: $${this.envio.toFixed(2).replace('.', ',')}</p>
+        <p>Total: ${this.calcularTotal()}</p>
       </div>
     `;
   }
