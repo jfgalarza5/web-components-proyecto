5000c855e0045e0525885c4c7126ec054f04071d
johngalarza1970 <johngalarza1970@gmail.com>
2025-07-09 16:30:05 -0500

    feat: crear componente base espe-resumen-compra

---
+++ components/espe-resumen-compra.js
@@ -0,0 +1,9 @@
+import { LitElement, html } from 'lit';
+
+class ESPEResumenCompra extends LitElement {
+  render() {
+    return html`<p>Resumen de compra</p>`;
+  }
+}
+
+customElements.define('espe-resumen-compra', ESPEResumenCompra);
